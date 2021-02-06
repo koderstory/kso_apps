47d5938e696ad79b88171a016187df21e100b417
Aldy A <aldy.ahsandin@gmail.com>
2021-02-06 15:13:27 +0700

    Update .gunicorn.sh

--- .gunicorn.sh
+++ .gunicorn.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-DEBUG_MODE=$(egrep -w DEBUG .env)
-cd src
-if [ "$DEBUG_MODE" = "DEBUG=True" ]; then
-        pipenv run gunicorn --env DJANGO_SETTINGS_MODULE=config.settings --env DJANGO_CONFIGURATION=Development  config.wsgi -b 0.0.0.0:8000
-else
-        pipenv run gunicorn --env DJANGO_SETTINGS_MODULE=config.settings --env DJANGO_CONFIGURATION=Production  config.wsgi -b 0.0.0.0:8000
-fi
 
+NAME="urdomain.com"
+DIR=/home/dev/urdomain.com
+USER=dev
+GROUP=dev
+WORKERS=3
+BIND=0.0.0.0:8080
+
+cd $DIR
+source .venv/bin/activate
+cd src
+
+
+exec ../.venv/bin/gunicorn config.wsgi \
+        --env DJANGO_SETTINGS_MODULE=config.settings \
+        --env DJANGO_CONFIGURATION=Development \
+        --bind $BIND \
+        --workers $WORKERS \
+        --name $NAME \
+        --group $GROUP
