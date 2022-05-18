3e4d7187471610e6be75f2ff3df9d27dde9b95d8
aldyahsn <aldy.ahsandin@gmail.com>
2022-05-18 07:14:48 +0700

    change server folder config to .server

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ read -p "2. Enter Domain?  (urdomain.com) " DOMAIN
 
 # set variables
 DIR=$(pwd)
-BIND="unix:"$DIR"/_server/.gunicorn.sock"
-GUNICORN=$DIR"/_server/.gunicorn.sh"
+BIND="unix:"$DIR"/.server/.gunicorn.sock"
+GUNICORN=$DIR"/.server/.gunicorn.sh"
 GUNICORN_ACCESS=$DIR"/logs/gunicorn-access.log"
 GUNICORN_ERROR=$DIR"/logs/gunicorn-errors.log"
 NGINX_ACCESS=$DIR"/logs/nginx-access.log"
@@ -15,34 +15,34 @@ NGINX_ERROR=$DIR"/logs/nginx-errors.log"
 
 cd $DIR
 
-sed -i "s/MYDOMAIN/$DOMAIN/g" _server/gunicorn.sh
-sed -i "s~MYDIR~$DIR~g" _server/gunicorn.sh
-sed -i "s/MYUSER/$USER/g" _server/gunicorn.sh
-sed -i "s~MYBIND~$BIND~g" _server/gunicorn.sh
-sed -i "s/MYDEBUG/$DEBUG/g" _server/gunicorn.sh
+sed -i "s/MYDOMAIN/$DOMAIN/g" .server/gunicorn.sh
+sed -i "s~MYDIR~$DIR~g" .server/gunicorn.sh
+sed -i "s/MYUSER/$USER/g" .server/gunicorn.sh
+sed -i "s~MYBIND~$BIND~g" .server/gunicorn.sh
+sed -i "s/MYDEBUG/$DEBUG/g" .server/gunicorn.sh
 
-sed -i "s/MYDOMAIN/$DOMAIN/g" _server/supervisor.conf
-sed -i "s~MYGUNICORN~$GUNICORN~g" _server/supervisor.conf
-sed -i "s~MYDIR~$DIR~g" _server/supervisor.conf
-sed -i "s/MYUSER/$USER/g" _server/supervisor.conf
-sed -i "s~MYLOG_ACCESS~$GUNICORN_ACCESS~g" _server/supervisor.conf
-sed -i "s~MYLOG_ERROR~$GUNICORN_ERROR~g" _server/supervisor.conf
+sed -i "s/MYDOMAIN/$DOMAIN/g" .server/supervisor.conf
+sed -i "s~MYGUNICORN~$GUNICORN~g" .server/supervisor.conf
+sed -i "s~MYDIR~$DIR~g" .server/supervisor.conf
+sed -i "s/MYUSER/$USER/g" .server/supervisor.conf
+sed -i "s~MYLOG_ACCESS~$GUNICORN_ACCESS~g" .server/supervisor.conf
+sed -i "s~MYLOG_ERROR~$GUNICORN_ERROR~g" .server/supervisor.conf
 
-sed -i "s/MYDOMAIN/$DOMAIN/g" _server/nginx.conf
-sed -i "s~MYLOG_ACCESS~$NGINX_ACCESS~g" _server/nginx.conf
-sed -i "s~MYLOG_ERROR~$NGINX_ERROR~g" _server/nginx.conf
-sed -i "s~MYBIND~$BIND~g" _server/nginx.conf
+sed -i "s/MYDOMAIN/$DOMAIN/g" .server/nginx.conf
+sed -i "s~MYLOG_ACCESS~$NGINX_ACCESS~g" .server/nginx.conf
+sed -i "s~MYLOG_ERROR~$NGINX_ERROR~g" .server/nginx.conf
+sed -i "s~MYBIND~$BIND~g" .server/nginx.conf
 
 
 if [[ -z "$WWW" || "$WWW" = "Y"  ]]; then
-    sed -i "s/WWWDOMAIN/www.$DOMAIN/g" _server/nginx.conf
+    sed -i "s/WWWDOMAIN/www.$DOMAIN/g" .server/nginx.conf
 else
-    sed -i 's/WWWDOMAIN/ /g' _server/nginx.conf
+    sed -i 's/WWWDOMAIN/ /g' .server/nginx.conf
 fi
 
-chmod u+x _server/gunicorn.sh
-mv _server/nginx.conf $DOMAIN"_nginx.conf"
-mv _server/supervisor.conf $DOMAIN".conf"
+chmod u+x .server/gunicorn.sh
+mv .server/nginx.conf $DOMAIN"_nginx.conf"
+mv .server/supervisor.conf $DOMAIN".conf"
 mv example.env .env
 
 sudo mv -i $DOMAIN /etc/nginx/sites-available/
