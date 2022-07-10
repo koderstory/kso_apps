b00d6fd76f3289a276a81a1b4e827b9a01a0769d
aldyahsn <aldy.ahsandin@gmail.com>
2022-07-10 09:41:50 +0700

    add custom user model:Account

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core.apps.CoreConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 # ------------------------
@@ -149,3 +150,7 @@ MEDIA_ROOT = BASE_DIR / 'public/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ------------
+# USER
+AUTH_USER_MODEL = 'authentication.Account'
