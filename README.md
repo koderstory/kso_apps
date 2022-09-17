d243dc8217f737c363f87a0b34b12e88bee0692b
aldyahsn <aldy.ahsandin@gmail.com>
2022-09-17 11:00:59 +0700

    init new remake

---
+++ README.md
@@ -0,0 +1,4 @@
+[] Server Setup
+[] Blog
+[] Menu
+[] Page Builder
---
+++ k5website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class K5WebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'k5website'
