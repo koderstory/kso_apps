d41600061028214a92b34b177a193b0ff9c0ba2e
aldyahsn <aldy.ahsandin@gmail.com>
2022-10-19 05:21:00 +0700

    create gunicorn and nginx config

--- install.sh
+++ install.sh
@@ -9,18 +9,68 @@ echo -n "Enter your domain name:"
 read DOMAIN
 
 # Creating the working directory
-cd 
-mkdir "$(pwd)/Websites/$DOMAIN"
-HOMEDIR="$(pwd)/Websites/$DOMAIN"
+HOMEDIR="$(pwd)/websites/$DOMAIN"
+mkdir -p "$HOMEDIR/.venv"
 cd $HOMEDIR
 
 # INSTALL DJANGO 
-mkdir .venv
 pipenv install django gunicorn
-source .venv/bin/activate
+source "$HOMEDIR/.venv/bin/activate"
 django-admin startproject config .
 
+# NGINX
+echo "Creating NGINX config file..."
+echo "server {
+    listen 80;
+    server_name $DOMAIN www.$DOMAIN;
+    error_log /var/log/nginx/$SITENAME.error.log;
+    access_log /var/log/nginx/$SITENAME.access.log;
+    rewrite_log on;
+    server_tokens off;
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection '1; mode=block';
+    
+    location /static/ {
+        root $HOMEDIR;
+	    expires 30d;
+        log_not_found off;
+        access_log off;
+    }
+    location /media/ {
+        root $HOMEDIR;
+	    expires 30d;
+        log_not_found off;
+        access_log off;
+     }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn_$DOMAIN.sock;
+    }
+}" | sudo tee /etc/nginx/sites-available/$DOMAIN.conf
+sudo ln -sf /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
+sudo systemctl restart nginx
 
+
+# SYSTEMD
+echo "Creating Gunicorn config file..."
+echo "[Unit]
+Description=gunicorn $DOMAIN daemon
+After=network.target
+
+[Service]
+User=$USER
+Group=www-data
+WorkingDirectory=${HOMEDIR}
+ExecStart=$HOMEDIR/.venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:/run/gunicorn_$DOMAIN.sock app.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/gunicorn_$DOMAIN.service
+
+# exit from the virtual environment and restart Gunicorn
+deactivate
+systemctl start gunicorn_$DOMAIN
+systemctl enable gunicorn_$DOMAIN
 
 
 
