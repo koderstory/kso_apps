6afdf8e3e47e4a0efaf3bc2ed29f854c153d625d
aldyahsn <aldy.ahsandin@gmail.com>
2022-10-23 06:57:04 +0000

    fix code

--- install.sh
+++ install.sh
@@ -14,31 +14,77 @@ mkdir -p "$HOMEDIR/.venv"
 cd $HOMEDIR
 
 # INSTALL DJANGO 
+sudo -H pip install -U pipenv
+
 pipenv install django gunicorn
 source "$HOMEDIR/.venv/bin/activate"
 django-admin startproject config .
 
-# NGINX
-echo "Creating NGINX config file..."
+
+echo "======================================="
+echo "Creating Systemd and Socket Config ..."
+echo "======================================="
+echo "\n"
+echo "[Unit]
+Description=gunicorn socket -> $DOMAIN
+
+[Socket]
+ListenStream=/run/gunicorn_$DOMAIN.sock
+
+[Install]
+WantedBy=sockets.target
+"| sudo tee /etc/systemd/system/gunicorn_$DOMAIN.socket
+
+echo "\n"
+echo "[Unit]
+Description=gunicorn $DOMAIN daemon
+Requires=gunicorn_$DOMAIN.socket
+After=network.target
+
+[Service]
+User=$USER
+Group=www-data
+WorkingDirectory=$HOMEDIR
+ExecStart=$HOMEDIR/.venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn_$DOMAIN.sock \
+          config.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/gunicorn_$DOMAIN.service
+
+sudo systemctl start gunicorn_$DOMAIN.socket
+sudo systemctl enable gunicorn_$DOMAIN.socket
+curl --unix-socket /run/gunicorn_$DOMAIN.sock localhost
+
+
+echo "\n\n\n"
+echo "======================================="
+echo "NGINX Config ..."
+echo "======================================="
+
+echo "\n"
 echo "server {
     listen 80;
     server_name $DOMAIN www.$DOMAIN;
-    error_log /var/log/nginx/$SITENAME.error.log;
-    access_log /var/log/nginx/$SITENAME.access.log;
+    error_log /var/log/nginx/.$DOMAIN_error.log;
+    access_log /var/log/nginx/.$DOMAIN_access.log;
     rewrite_log on;
     server_tokens off;
     add_header X-Content-Type-Options nosniff;
     add_header X-XSS-Protection '1; mode=block';
-    
+
     location /static/ {
         root $HOMEDIR;
-	    expires 30d;
+            expires 30d;
         log_not_found off;
         access_log off;
     }
     location /media/ {
         root $HOMEDIR;
-	    expires 30d;
+            expires 30d;
         log_not_found off;
         access_log off;
      }
@@ -46,31 +92,7 @@ echo "server {
         include proxy_params;
         proxy_pass http://unix:/run/gunicorn_$DOMAIN.sock;
     }
-}" | sudo tee /etc/nginx/sites-available/$DOMAIN.conf
-sudo ln -sf /etc/nginx/sites-available/$DOMAIN.conf /etc/nginx/sites-enabled/$DOMAIN.conf
+}
+" | sudo tee /etc/nginx/sites-available/$DOMAIN
+sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
 sudo systemctl restart nginx
-
-
-# SYSTEMD
-echo "Creating Gunicorn config file..."
-echo "[Unit]
-Description=gunicorn $DOMAIN daemon
-After=network.target
-
-[Service]
-User=$USER
-Group=www-data
-WorkingDirectory=${HOMEDIR}
-ExecStart=$HOMEDIR/.venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:/run/gunicorn_$DOMAIN.sock app.wsgi:application
-
-[Install]
-WantedBy=multi-user.target
-" | sudo tee /etc/systemd/system/gunicorn_$DOMAIN.service
-
-# exit from the virtual environment and restart Gunicorn
-deactivate
-systemctl start gunicorn_$DOMAIN
-systemctl enable gunicorn_$DOMAIN
-
-
-
