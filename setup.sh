9e7ae9070f1a6dcc05707ee6d3dafcfffd0926d5
aldyahsn <aldy.ahsandin@gmail.com>
2022-10-24 12:06:10 +0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Generate passowrds
+DBPASS=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12`
+#SFTPPASS=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12`
+
+# Input domain name
+echo -ne "Enter your domain name:"
+read DOMAIN
+
+HOMEDIR="$(pwd)/websites/$DOMAIN"
+mkdir -p "$HOMEDIR/.venv"
+cd $HOMEDIR
+
+# -------------------------------------------------------------
+sudo -H pip install -U pipenv
+
+pipenv install django gunicorn django-environ psycopg2 pillow whitenoise djlint
+source "$HOMEDIR/.venv/bin/activate"
+django-admin startproject config .
+
+echo "[Unit]
+Description=gunicorn socket -> $DOMAIN
+
+[Socket]
+ListenStream=/run/gunicorn_$DOMAIN.sock
+
+[Install]
+WantedBy=sockets.target
+"| sudo tee /etc/systemd/system/gunicorn_$DOMAIN.socket >> $HOMEDIR/deploy.log
+
+# -----------------------------------------
+sudo rm -r /etc/systemd/system/gunicorn_$DOMAIN.service >> /dev/null
+echo "[Unit]
+Description=gunicorn $DOMAIN daemon
+Requires=gunicorn_$DOMAIN.socket
+After=network.target
+
+[Service]
+User=$USER
+Group=www-data
+WorkingDirectory=$HOMEDIR
+ExecStart=$HOMEDIR/.venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn_$DOMAIN.sock \
+          config.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/gunicorn_$DOMAIN.service >> $HOMEDIR/deploy.log
+
+# -----------------------------------------------------
+sudo systemctl start gunicorn_$DOMAIN.socket
+sudo systemctl enable gunicorn_$DOMAIN.socket
+curl --unix-socket /run/gunicorn_$DOMAIN.sock localhost >> $HOMEDIR/deploy.log
+sudo systemctl daemon-reload
+
+# -------------------------------------------------------------
+sudo rm -f /etc/nginx/sites-available/$DOMAIN >> /dev/null
+sudo rm -f /etc/nginx/sites-enabled/$DOMAIN >> /dev/null
+echo "server {
+    listen 80;
+    server_name $DOMAIN www.$DOMAIN;
+    error_log /var/log/nginx/.$DOMAIN_error.log;
+    access_log /var/log/nginx/.$DOMAIN_access.log;
+    rewrite_log on;
+    server_tokens off;
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection '1; mode=block';
+
+    location /static/ {
+        root $HOMEDIR;
+            expires 30d;
+        log_not_found off;
+        access_log off;
+    }
+    location /media/ {
+        root $HOMEDIR;
+            expires 30d;
+        log_not_found off;
+        access_log off;
+     }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn_$DOMAIN.sock;
+    }
+}
+" | sudo tee /etc/nginx/sites-available/$DOMAIN >> $HOMEDIR/deploy.log
+sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+
+
+# -------------------------------------------------------------
+printf "✅✅✅✅✅  INSTALLATION COMPLETE  ✅✅✅✅✅\n\n"
+
+
+
