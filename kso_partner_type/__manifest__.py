647058f1366bae8fdd4724b1e5e7df8ae6ccdffd
aldyahsn <aldy.ahsandin@gmail.com>
2025-07-21 13:04:39 +0700

    add partner type

---
+++ kso_partner_type/__manifest__.py
@@ -0,0 +1,16 @@
+# partner_flags/__manifest__.py
+{
+    "name": "Partner Customer/Vendor Flags",
+    "version": "18.0.1.0.0",
+    "summary": "Add Customer and Vendor check-boxes to partners",
+    "category": "Custom",
+    "author": "Your Name",
+    "depends": ["base", "account"],
+    "data": [
+        "views/res_partner_type.xml",
+        # "views/res_partner_kanban_customer_filter.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
