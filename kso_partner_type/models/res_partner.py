e7911ae7334b7c92584cad805832898a8ca95438
aldyahsn <aldy.ahsandin@gmail.com>
2025-07-21 13:59:42 +0700

    fixing script

--- kso_partner_type/models/res_partner.py
+++ kso_partner_type/models/res_partner.py
@@ -1,25 +1,25 @@
 from odoo import models, fields, api
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+# class ResPartner(models.Model):
+#     _inherit = 'res.partner'
 
-    is_customer = fields.Boolean(
-        string='Customer',
-        compute='_compute_is_customer',
-        store=True,
-    )
-    is_vendor = fields.Boolean(
-        string='Vendor',
-        compute='_compute_is_vendor',
-        store=True,
-    )
+    # is_customer = fields.Boolean(
+    #     string='Customer',
+    #     compute='_compute_is_customer',
+    #     store=True,
+    # )
+    # is_vendor = fields.Boolean(
+    #     string='Vendor',
+    #     compute='_compute_is_vendor',
+    #     store=True,
+    # )
 
-    @api.depends('customer_rank')
-    def _compute_is_customer(self):
-        for partner in self:
-            partner.is_customer = partner.customer_rank > 0
+    # @api.depends('customer_rank')
+    # def _compute_is_customer(self):
+    #     for partner in self:
+    #         partner.is_customer = partner.customer_rank > 0
 
-    @api.depends('supplier_rank')
-    def _compute_is_vendor(self):
-        for partner in self:
-            partner.is_vendor = partner.supplier_rank > 0
+    # @api.depends('supplier_rank')
+    # def _compute_is_vendor(self):
+    #     for partner in self:
+    #         partner.is_vendor = partner.supplier_rank > 0
